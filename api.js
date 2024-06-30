ee346afb12091c4b6f9b50cad521c53287e4b74e
victor <victor.falva08@gmail.com>
2024-06-30 20:42:07 -0300

    update package info

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require("express")
 const api = express()
 // Inicialização da API com Express
 
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
 const caixaPath = require("./caixaEletronico")
 // Importação do bodyParser para tratamento de requisições JSON e da rota onde está a implementação da função que fará o cálculo da quantidade de cédulas
 
@@ -11,7 +11,7 @@ api.use(bodyParser.json()) // Middleware para interpretar o corpo (body) da requ
 api.use((error, req, res, next) => {
     if(error) {
         console.log(error)
-        return res.status(400).json({ "Erro": "A API não foi capaz de processar a requisição. Verifique se o JSON fornecido está no formato adequado." });
+        return res.status(400).json({ "Erro": "A API não foi capaz de processar a requisição. Verifique se o JSON fornecido está no formato adequado." })
     }
 }) // Este Middleware verifica qualquer erro imprevisto no formato da requisição
 
