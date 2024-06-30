762e2455d5a27a8ca052f6a2c90b5ad9ddd04870
victor <victor.falva08@gmail.com>
2024-06-30 20:46:33 -0300

    final adjustments in documentation

--- api.js
+++ api.js
@@ -1,21 +1,21 @@
 const express = require("express")
 const api = express()
-// Inicialização da API com Express
+// inicializacao da api com express
 
 const bodyParser = require("body-parser")
 const caixaPath = require("./caixaEletronico")
-// Importação do bodyParser para tratamento de requisições JSON e da rota onde está a implementação da função que fará o cálculo da quantidade de cédulas
+// Importacao do bodyParser para tratamento de requisicoes JSON e da rota onde esta a implementacao da funcao que fara o calculo da quantidade de cedulas
 
-api.use(bodyParser.json()) // Middleware para interpretar o corpo (body) da requisição como um JSON
+api.use(bodyParser.json()) // Middleware para interpretar o corpo (body) da requisicao como um JSON
 
 api.use((error, req, res, next) => {
     if(error) {
         console.log(error)
         return res.status(400).json({ "Erro": "A API não foi capaz de processar a requisição. Verifique se o JSON fornecido está no formato adequado." })
     }
-}) // Este Middleware verifica qualquer erro imprevisto no formato da requisição
+}) // Este Middleware verifica qualquer erro imprevisto no formato da requisicao
 
-api.post('/api/saque', caixaPath) // Requisição do método POST para o path especificado, realizando a chamada da função em caixaPath
+api.post('/api/saque', caixaPath) // Requisicao do metodo POST para o path especificado, realizando a chamada da funcao em caixaPath
 
 const port = 5000
 api.listen(port, () => {
